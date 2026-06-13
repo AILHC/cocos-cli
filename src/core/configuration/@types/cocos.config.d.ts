85f168400bb6e23906dcdb4ef35b38340c7f0c5c
AILHC <505126057@qq.com>
2026-06-13 21:49:01 +0800

    docs: document cocos config overlay ownership

--- src/core/configuration/@types/cocos.config.d.ts
+++ src/core/configuration/@types/cocos.config.d.ts
@@ -1,19 +1,8 @@
 import { ImportConfiguration } from '../../assets/@types/config-export';
-import { BuildConfiguration } from '../../builder/@types/config-export';
-import { IEngineConfig } from '../../engine/@types/config';
-import { ScriptProjectConfig } from '../../scripting/@types/config-export';
-
-export interface SceneConfiguration {
-    [key: string]: any;
-}
 
 // 用于 schema 校验规则导出
 export interface COCOS_CONFIG {
     $schema?: string;
     version: string;
-    builder: BuildConfiguration;
-    import: ImportConfiguration;
-    engine: IEngineConfig;
-    scene?: SceneConfiguration;
-    script: ScriptProjectConfig;
+    import?: Pick<ImportConfiguration, 'restoreAssetDBFromCache' | 'globList' | 'createTemplateRoot'>;
 }
